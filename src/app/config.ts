f7062c6eeff33c64c84a30b1bfa8901bc6ba0f58
Abduselamjibril <abduselamjibril78@gmail.com>
2026-08-20 13:59:26 +0300

    feat: add environment configuration file for server and API endpoints

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  //serverUrl: 'https://ihsan.skylinkict.com',
-  //apiBase: 'https://ihsan.skylinkict.com/api'
-  serverUrl: 'http://localhost:8585',
-  apiBase: 'http://localhost:8585/api'
+  serverUrl: 'https://ihsan.skylinkict.com',
+  apiBase: 'https://ihsan.skylinkict.com/api'
+  //serverUrl: 'http://localhost:8585',
+  //apiBase: 'http://localhost:8585/api'
 };
